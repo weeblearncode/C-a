d22c0d9cb73954cf986a394b341616d7800ea165
Lê Nguyễn Thiên Phúc <144975595+weeblearncode@users.noreply.github.com>
2025-04-23 12:30:31 +0700

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.Input = new System.Windows.Forms.Label();
+            this.Valuelabel = new System.Windows.Forms.Label();
+            this.Encodebutton = new System.Windows.Forms.Button();
+            this.Sortb = new System.Windows.Forms.Button();
+            this.Textanswer = new System.Windows.Forms.Label();
+            this.ASCII = new System.Windows.Forms.ListBox();
+            this.Textinput = new System.Windows.Forms.TextBox();
+            this.N = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Inputcode = new System.Windows.Forms.Button();
+            this.Outputcode = new System.Windows.Forms.Button();
+            this.Sort = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.AutoSize = true;
+            this.Input.Location = new System.Drawing.Point(12, 20);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(121, 16);
+            this.Input.TabIndex = 1;
+            this.Input.Text = "Input data (A-Z <40)";
+            // 
+            // Valuelabel
+            // 
+            this.Valuelabel.AutoSize = true;
+            this.Valuelabel.Location = new System.Drawing.Point(156, 20);
+            this.Valuelabel.Name = "Valuelabel";
+            this.Valuelabel.Size = new System.Drawing.Size(90, 16);
+            this.Valuelabel.TabIndex = 2;
+            this.Valuelabel.Text = "Encode value";
+            // 
+            // Encodebutton
+            // 
+            this.Encodebutton.Location = new System.Drawing.Point(24, 114);
+            this.Encodebutton.Name = "Encodebutton";
+            this.Encodebutton.Size = new System.Drawing.Size(100, 42);
+            this.Encodebutton.TabIndex = 4;
+            this.Encodebutton.Text = "Encode";
+            this.Encodebutton.UseVisualStyleBackColor = true;
+            this.Encodebutton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Sortb
+            // 
+            this.Sortb.Location = new System.Drawing.Point(159, 114);
+            this.Sortb.Name = "Sortb";
+            this.Sortb.Size = new System.Drawing.Size(95, 42);
+            this.Sortb.TabIndex = 5;
+            this.Sortb.Text = "Sort";
+            this.Sortb.UseVisualStyleBackColor = true;
+            this.Sortb.Click += new System.EventHandler(this.Sortb_Click_1);
+            // 
+            // Textanswer
+            // 
+            this.Textanswer.AutoSize = true;
+            this.Textanswer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Textanswer.Location = new System.Drawing.Point(362, 49);
+            this.Textanswer.Name = "Textanswer";
+            this.Textanswer.Size = new System.Drawing.Size(79, 18);
+            this.Textanswer.TabIndex = 6;
+            this.Textanswer.Text = "Answer = \" \"";
+            // 
+            // ASCII
+            // 
+            this.ASCII.FormattingEnabled = true;
+            this.ASCII.ItemHeight = 16;
+            this.ASCII.Location = new System.Drawing.Point(362, 114);
+            this.ASCII.Name = "ASCII";
+            this.ASCII.Size = new System.Drawing.Size(120, 100);
+            this.ASCII.TabIndex = 7;
+            // 
+            // Textinput
+            // 
+            this.Textinput.Location = new System.Drawing.Point(15, 49);
+            this.Textinput.MaxLength = 40;
+            this.Textinput.Name = "Textinput";
+            this.Textinput.Size = new System.Drawing.Size(118, 22);
+            this.Textinput.TabIndex = 8;
+            // 
+            // N
+            // 
+            this.N.Location = new System.Drawing.Point(159, 49);
+            this.N.Name = "N";
+            this.N.Size = new System.Drawing.Size(120, 22);
+            this.N.TabIndex = 10;
+            this.N.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 39);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Print";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Inputcode
+            // 
+            this.Inputcode.Location = new System.Drawing.Point(159, 162);
+            this.Inputcode.Name = "Inputcode";
+            this.Inputcode.Size = new System.Drawing.Size(95, 39);
+            this.Inputcode.TabIndex = 11;
+            this.Inputcode.Text = "Input Code";
+            this.Inputcode.UseVisualStyleBackColor = true;
+            this.Inputcode.Click += new System.EventHandler(this.Inputcode_Click_1);
+            // 
+            // Outputcode
+            // 
+            this.Outputcode.Location = new System.Drawing.Point(159, 207);
+            this.Outputcode.Name = "Outputcode";
+            this.Outputcode.Size = new System.Drawing.Size(95, 40);
+            this.Outputcode.TabIndex = 12;
+            this.Outputcode.Text = "Output Code";
+            this.Outputcode.UseVisualStyleBackColor = true;
+            this.Outputcode.Click += new System.EventHandler(this.Outputcode_Click_1);
+            // 
+            // Sort
+            // 
+            this.Sort.AutoSize = true;
+            this.Sort.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Sort.Location = new System.Drawing.Point(362, 81);
+            this.Sort.Name = "Sort";
+            this.Sort.Size = new System.Drawing.Size(59, 18);
+            this.Sort.TabIndex = 13;
+            this.Sort.Text = "Sort = \" \"";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(519, 252);
+            this.Controls.Add(this.Sort);
+            this.Controls.Add(this.Outputcode);
+            this.Controls.Add(this.Inputcode);
+            this.Controls.Add(this.N);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Textinput);
+            this.Controls.Add(this.ASCII);
+            this.Controls.Add(this.Textanswer);
+            this.Controls.Add(this.Sortb);
+            this.Controls.Add(this.Encodebutton);
+            this.Controls.Add(this.Valuelabel);
+            this.Controls.Add(this.Input);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Comp 1551";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label Input;
+        private System.Windows.Forms.Label Valuelabel;
+        private System.Windows.Forms.Button Encodebutton;
+        private System.Windows.Forms.Button Sortb;
+        private System.Windows.Forms.ListBox ASCII;
+        private System.Windows.Forms.TextBox Textinput;
+        public System.Windows.Forms.Label Textanswer;
+        private System.Windows.Forms.TextBox N;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Inputcode;
+        private System.Windows.Forms.Button Outputcode;
+        public System.Windows.Forms.Label Sort;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string _inputString;
+        private int _offset;
+        private string _encodedResult;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Set the InputString and Offset properties
+                _inputString = Textinput.Text;
+                _offset = Convert.ToInt32(N.Text);
+
+                // Transform the input string using the Encode method
+                _encodedResult = Encode(_inputString, _offset);
+
+                // Notify the user that the encoding is complete
+                MessageBox.Show("Encoding complete. Click 'Print' to display the result.",
+                                "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // Handle any unexpected exceptions
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string Encode(string input, int offset)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (char c in input)
+            {
+                int asciiValue = (char)((c - 'A' + offset + 26) % 26 + 'A');
+                result.Append((char)asciiValue);
+            }
+            return result.ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_encodedResult))
+            {
+                // Display the stored encoded result in the Textanswer label
+                Textanswer.Text = _encodedResult;
+            }
+            else
+            {
+                // Notify the user that no encoding has been performed yet
+                MessageBox.Show("No encoded result available. Please click 'Encode' first.",
+                                "No Result", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Sortb_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get the input string from the Textinput TextBox
+                string input = Textinput.Text;
+
+                // Validate that the input is not empty
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    MessageBox.Show("Please enter a valid input string.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Sort the input string alphabetically
+                string sortedInput = new string(input.OrderBy(c => c).ToArray());
+
+                // Display the sorted result in the Sort label
+                Sort.Text = $"Sort = \"{sortedInput}\"";
+            }
+            catch (Exception ex)
+            {
+                // Handle any unexpected errors
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DisplayAsciiInListBox(string input)
+        {
+            // Clear the listBox1 before adding new items
+            ASCII.Items.Clear();
+
+            // Convert each character to its ASCII code and add to the listBox1
+            foreach (char c in input)
+            {
+                ASCII.Items.Add($"{c} -> {((int)c)}");
+            }
+        }
+
+        private void Inputcode_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get the input string from the Textinput TextBox
+                string input = Textinput.Text;
+
+                // Call the DisplayAsciiInListBox function to show ASCII codes in listBox1
+                DisplayAsciiInListBox(input);
+            }
+            catch (Exception ex)
+            {
+                // Handle any unexpected errors
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Outputcode_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get all items from listBox1 and concatenate them into a single string
+                string output = string.Join(Environment.NewLine, ASCII.Items.Cast<string>());
+
+                // Display the concatenated string in a MessageBox
+                MessageBox.Show(output, "ASCII Codes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // Handle any unexpected errors
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    public class StringEncoder
+    {
+        private string _inputString;
+        private int _offset;
+
+        // Property for InputString with validation
+        public string InputString
+        {
+            get => _inputString;
+            set
+            {
+                if (!string.IsNullOrEmpty(value) && value.All(char.IsUpper) && value.Length <= 40)
+                {
+                    _inputString = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Input string must contain only capital letters (A-Z) and be at most 40 characters long.");
+                }
+            }
+        }
+
+        // Property for Offset with validation
+        public int Offset
+        {
+            get => _offset;
+            set
+            {
+                if (value >= -25 && value <= 25)
+                {
+                    _offset = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Offset must be between -25 and 25.");
+                }
+            }
+        }
+
+        // Method to encode the input string
+        public string Encode()
+        {
+            if (string.IsNullOrEmpty(_inputString))
+            {
+                throw new InvalidOperationException("Input string is not set.");
+            }
+
+            StringBuilder result = new StringBuilder();
+            foreach (char c in _inputString)
+            {
+                int asciiValue = (char)((c - 'A' + _offset + 26) % 26 + 'A');
+                result.Append((char)asciiValue);
+            }
+            return result.ToString();
+        }
+
+        // Method to get ASCII representation of the input string
+        public string[] GetAsciiArray()
+        {
+            if (string.IsNullOrEmpty(_inputString))
+            {
+                throw new InvalidOperationException("Input string is not set.");
+            }
+
+            return _inputString.Select(c => $"{c} -> {((int)c)}").ToArray();
+        }
+    }
+
+    public class StringProcessing
+    {
+        private string _inputString;
+        private int _offset;
+
+        // Property for InputString with validation
+        public string InputString
+        {
+            get => _inputString;
+            set
+            {
+                if (!string.IsNullOrEmpty(value) && value.All(char.IsUpper) && value.Length <= 40)
+                {
+                    _inputString = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Input string must contain only capital letters (A-Z) and be at most 40 characters long.");
+                }
+            }
+        }
+
+        // Property for Offset with validation
+        public int Offset
+        {
+            get => _offset;
+            set
+            {
+                if (value >= -25 && value <= 25)
+                {
+                    _offset = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Offset must be between -25 and 25.");
+                }
+            }
+        }
+
+        // Method to encode the input string
+        public string Encode()
+        {
+            if (string.IsNullOrEmpty(_inputString))
+            {
+                throw new InvalidOperationException("Input string is not set.");
+            }
+
+            StringBuilder result = new StringBuilder();
+            foreach (char c in _inputString)
+            {
+                int asciiValue = (char)((c - 'A' + _offset + 26) % 26 + 'A');
+                result.Append((char)asciiValue);
+            }
+            return result.ToString();
+        }
+
+        // Method to sort the input string alphabetically
+        public string SortString()
+        {
+            if (string.IsNullOrEmpty(_inputString))
+            {
+                throw new InvalidOperationException("Input string is not set.");
+            }
+
+            return new string(_inputString.OrderBy(c => c).ToArray());
+        }
+
+        // Method to get ASCII representation of the input string
+        public string[] GetAsciiArray()
+        {
+            if (string.IsNullOrEmpty(_inputString))
+            {
+                throw new InvalidOperationException("Input string is not set.");
+            }
+
+            return _inputString.Select(c => $"{c} -> {((int)c)}").ToArray();
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                // Test 1: Encode and Decode
+                Console.WriteLine("Enter input string (capital letters only, max 40 characters):");
+                string inputString = Console.ReadLine();
+                Console.WriteLine("Enter offset value (-25 to 25):");
+                int offset = int.Parse(Console.ReadLine());
+
+                // Instantiate the StringProcessing class
+                StringProcessing processor = new StringProcessing
+                {
+                    InputString = inputString,
+                    Offset = offset
+                };
+
+                // Encode the input string
+                string encodedResult = processor.Encode();
+                Console.WriteLine($"Encoded Result: {encodedResult}");
+
+                // Decode the encoded result
+                processor.InputString = encodedResult;
+                processor.Offset = -offset; // Reverse the offset
+                string decodedResult = processor.Decode();
+                Console.WriteLine($"Decoded Result: {decodedResult}");
+
+                // Verify if the decoded result matches the original input
+                Console.WriteLine(decodedResult == inputString
+                    ? "Test Passed: Decoded result matches the original input."
+                    : "Test Failed: Decoded result does not match the original input.");
+
+                // Test 2: Sort the input string
+                processor.InputString = inputString; // Reset to original input
+                string sortedResult = processor.SortString();
+                Console.WriteLine($"Sorted Result: {sortedResult}");
+
+                // Test 3: Get ASCII representation
+                string[] asciiArray = processor.GetAsciiArray();
+                Console.WriteLine("ASCII Codes:");
+                foreach (string ascii in asciiArray)
+                {
+                    Console.WriteLine(ascii);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+    }
+
